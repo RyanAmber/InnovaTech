ccfde8bacd71480e76002a73bef30ba93aad49e1
RyanAmber <ryandiehl2027@gmail.com>
2025-05-25 17:35:53 -0400

    Update WebServer.java

--- WebServer.java
+++ WebServer.java
@@ -7,10 +7,25 @@ public class WebServer {
         port(4567);
 
         // Allow requests from anywhere (CORS)
-        before((req, res) -> {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Methods", "GET,POST");
-            res.header("Access-Control-Allow-Headers", "Content-Type");
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.header("Access-Control-Request-Method", "*");
+            response.header("Access-Control-Allow-Headers", "*");
+            response.type("application/json");
         });
 
         // Route to handle login
